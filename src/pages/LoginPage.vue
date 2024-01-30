e4484fe296f383e02282837dc0177825ab14c803
Dian Ramdhani Lubis <dian.lubis@prosa.ai>
2024-01-30 14:12:14 +0700

    init LoginPage

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <q-form>
+      <q-input label="Username" name="username" v-model="username" />
+      <q-input label="Password" name="password" type="password" v-model="password" />
+      <q-btn type="submit">Login</q-btn>
+      <q-btn flat></q-btn>
+    </q-form>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref('')
+const password = ref('')
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,8 +3,11 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
